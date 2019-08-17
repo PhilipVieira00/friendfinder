1b8b9dec62ba409605688a524df3f580d9f8a793
Philip Vieira <philipvieira00@gmail.com>
2019-08-17 16:34:29 -0400

    Added basic HTML to the HTML files

--- app/data/friends.js
+++ app/data/friends.js
@@ -7,14 +7,3 @@ function Friend(name, photo, scores) {
 friendArray = [{
 
 }];
-app.post("/api/friends", function(req, res) {
-    var newFriend = req.body;
-    newFriend.routeName = newFriend.name.replace(/\s+/g, "").toLowerCase();
-    console.log(newFriend);
-    friendArray.push(newFriend);
-    res.json(newFriend);
-})
-
-app.get("/api/friends", function(req, res) {
-    return res.json(friendArray);
-})
---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,11 @@
+app.post("/api/friends", function(req, res) {
+    var newFriend = req.body;
+    newFriend.routeName = newFriend.name.replace(/\s+/g, "").toLowerCase();
+    console.log(newFriend);
+    friendArray.push(newFriend);
+    res.json(newFriend);
+})
+
+app.get("/api/friends", function(req, res) {
+    return res.json(friendArray);
+})
